16a292903c2207214b9b887e47ad8ea72a75ce57
Ricardo C <ricardo@suggestic.com>
2018-08-13 17:33:41 -0500

    add firestore stream for StreamBuilder

--- firestoretest/lib/main.dart
+++ firestoretest/lib/main.dart
@@ -1,8 +1,11 @@
+//package 'rcc.flutter.modelogger';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "modelogger app",
     home: ModelList(),
   ));
@@ -13,11 +16,17 @@ class ModelList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10.0),
-      child: ListView.builder(
-        itemExtent: 80.0,
-        itemCount: 3,
-        itemBuilder: (context, i) => Text("$i"),
-      ),
+      color: Colors.white10,
+      child: StreamBuilder(
+          stream: Firestore.instance.collection('modelsmetainfo').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return Text("cargando...");
+            return ListView.builder(
+              itemExtent: 80.0,
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, i) => Text("$snapshot.data.documents[i]"),
+            );
+          }),
     );
   }
 }
