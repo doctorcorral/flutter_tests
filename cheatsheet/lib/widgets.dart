be8c7c21d3805ffbf8cdcd91acac58cfc27591f3
Ricardo C <ricardo@suggestic.com>
2018-08-23 14:38:51 -0500

    add widgets

--- cheatsheet/lib/widgets.dart
+++ cheatsheet/lib/widgets.dart
@@ -32,3 +32,45 @@ Widget rowTestSpaceEvenly() {
     ],
   );
 }
+
+Widget rowTestSpaceAround() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: <Widget>[
+      new Icon(Icons.star, size: 50.0),
+      new Icon(Icons.star, size: 50.0),
+      new Icon(Icons.star, size: 50.0),
+    ],
+  );
+}
+
+Widget rowTestText(BuildContext context) {
+  return Container(
+    color: Colors.amber,
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
+      children: <Widget>[
+        Text(
+          'Baseline',
+          style: Theme.of(context).textTheme.display3,
+        ),
+        Text(
+          'Baseline',
+          style: Theme.of(context).textTheme.body1,
+        ),
+      ],
+    ),
+  );
+}
+
+Widget rowTestSizes() {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      new Icon(Icons.star, size: 50.0),
+      new Icon(Icons.star, size: 200.0),
+      new Icon(Icons.star, size: 50.0),
+    ],
+  );
+}
