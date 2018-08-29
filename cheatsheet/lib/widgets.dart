3b4a8b97fce55b723c81f607c362aaa44374f1a5
Ricardo C <ricardo@suggestic.com>
2018-08-28 19:49:54 -0500

    add positioned2 Widget feat. constraints.maxHeight

--- cheatsheet/lib/widgets.dart
+++ cheatsheet/lib/widgets.dart
@@ -155,3 +155,27 @@ Widget positioned() {
     ),
   );
 }
+
+Widget positioned2() {
+  const iconSize = 50.0;
+  return Scaffold(
+    appBar: AppBar(title: Text('Stack with LayoutBuilder')),
+    body: LayoutBuilder(
+      builder: (context, constraints) => Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Material(color: Colors.yellowAccent),
+              Positioned(
+                top: 0.0,
+                child: Icon(Icons.star, size: iconSize),
+              ),
+              Positioned(
+                top: constraints.maxHeight - iconSize,
+                left: constraints.maxWidth - iconSize,
+                child: Icon(Icons.call, size: iconSize),
+              ),
+            ],
+          ),
+    ),
+  );
+}
