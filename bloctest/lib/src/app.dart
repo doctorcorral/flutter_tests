e9079d9fd84951634ed9ebc5e80b655530dd1ba1
Ricardo C <ricardo@suggestic.com>
2018-09-04 19:11:04 -0500

    add App body

--- bloctest/lib/src/app.dart
+++ bloctest/lib/src/app.dart
@@ -6,6 +6,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: Scaffold(
         body: MovieList(),
