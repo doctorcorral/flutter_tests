6268e70ae6a3fd404270786d708c9b976117d451
Ricardo C <ricardo@suggestic.com>
2018-10-02 18:31:11 -0500

    add codebase

---
+++ cardtest/README.md
@@ -0,0 +1,8 @@
+# cardtest
+
+A new Flutter project.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ cardtest/lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+  double height = MediaQuery.of(context).size.width;
+
+    return Card(
+      color: Colors.transparent,
+      elevation: 4.0,
+      child: new Container(
+        width: width / 1.2,
+        height: height / 1.7,
+        decoration: new BoxDecoration(
+          color: Colors.white,
+          borderRadius: new BorderRadius.circular(8.0),
+        ),
+        child: new Column(
+          children: <Widget>[
+            new Container(
+              width: width / 1.2,
+              height: height / 2.2,
+              decoration: new BoxDecoration(
+                borderRadius: new BorderRadius.only(
+                    topLeft: new Radius.circular(8.0),
+                    topRight: new Radius.circular(8.0)),
+                image: img,
+              ),
+            ),
+            new Container(
+                width: screenSize.width / 1.2,
+                height: screenSize.height / 1.7 - screenSize.height / 2.2,
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    new SwipeButton(
+                      text: "NOPE",
+                      onClick: swipeLeft,
+                    ),
+                    new SwipeButton(
+                      text: "LIKE",
+                      onClick: swipeRight,
+                    ),
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
