d52faf42d942566ddaa34a6a34d874f615a85699
Ricardo C <ricardo@suggestic.com>
2018-11-09 12:53:03 -0600

    add first functional TextField

--- textfieldtest/lib/main.dart
+++ textfieldtest/lib/main.dart
@@ -7,19 +7,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  Scaffold(body: TextFielder())
+      
     );
   }
 }
@@ -32,7 +26,12 @@ class _TextFielderState extends State<TextFielder> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: TextField()
-    );
+       child: Column(
+         children: <Widget>[ 
+           Text("Yaaay", style: TextStyle(fontSize: 34.0),),
+           TextField(onChanged: (s) => print(s))
+           ],
+        
+    ));
   }
 }
